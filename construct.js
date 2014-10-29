80c7e6b5974e313b3df0c48d3be8677e48bd970a
Nick Dugger <email@nickdugger.com>
2014-10-29 11:19:23 -0500

    Update construct.js

--- construct.js
+++ construct.js
@@ -1,22 +1,31 @@
 (function($) {
 	$.construct = function(children, parent) {
-		if (!parent) {
-			parent = $("<div>");
-		} else {
-			children = children.children
-		};
-		if (children) {
-			$.each(children, function() {
-				var child = $("<" + this.tag + ">");
-				$.each(this, function(key, value) {
-					if (child[key]) {
-						child[key](value);
+		if (typeof children === "object") {
+			if (!parent) {
+				parent = $("<div>");
+			} else {
+				children = children.children
+			};
+			if (!$.isArray(children)) {
+				children = [children]
+			};
+			if ($.isArray(children)) {
+				$.each(children, function() {
+					var child = $("<" + (this.tag || "div") + ">");
+					$.each(this, function(key, value) {
+						if (child[key]) {
+							child[key](value);
+						};
+					});
+					parent.append(child);
+					if (this.children) {
+						$.construct(this, child);
 					};
 				});
-				parent.append(child);
-				$.construct(this, child);
-			});
+			};
+			return parent.children();
+		} else {
+			console.error(typeof children + ": " + children + " is an invalid argument.");
 		};
-		return parent.children();
-	}
+	};
 })(jQuery);
